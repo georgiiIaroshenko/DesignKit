0ee11bb039c6e62cafdf3f92eb40fd8e26a5e7dd
georgiiIaroshenko <georg2991@mail.ru>
2025-02-05 21:28:02 +0300

    no message

--- DesignKit/Package.swift
+++ DesignKit/Package.swift
@@ -1,9 +1,9 @@
-// swift-tools-version: 5.6
+// swift-tools-version: 5.8
 import PackageDescription
 
 let package = Package(
     name: "DesignKit",
-    platforms: [.iOS(.v14)],
+    platforms: [.iOS(.v16)],
     products: [
         .library(name: "DesignKit", targets: ["DesignKit"]),
     ],
