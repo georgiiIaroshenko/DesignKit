fe30c3a383ee043f27d9b0fd16f5fbc23f405db5
georgiiIaroshenko <georg2991@mail.ru>
2025-02-05 21:55:10 +0300

    no message

--- Sources/DesignKit/Molecules/Onboarding.swift
+++ Sources/DesignKit/Molecules/Onboarding.swift
@@ -1,12 +1,12 @@
 import UIKit
 
 protocol OnboardingUIProtocol {
-    static func createNextScreenButton(text: String, action: @escaping () -> Void) -> UIButton
+    func createNextScreenButton(text: String, action: @escaping () -> Void) -> UIButton
 }
 
 public struct OnboardingUI: OnboardingUIProtocol {
     public init(){}
-    static func createNextScreenButton(text: String, action: @escaping () -> Void) -> UIButton {
+    public func createNextScreenButton(text: String, action: @escaping () -> Void) -> UIButton {
         let font = FontFactory(font: .bold, size: .midium, color: .green100, alignment: .center)
         return ActionButton.init(text: text, frontText: font, style: .positive, cornerRadius: .minimum, backgroundColor: .green300, action: action).createButton()
     }
