cf702616850d060a64c112fd926b6026bb8d75e0
georgiiIaroshenko <georg2991@mail.ru>
2025-02-07 19:43:48 +0300

    no message

--- Sources/DesignKit/Atoms/Radius.swift
+++ Sources/DesignKit/Atoms/Radius.swift
@@ -1,11 +1,15 @@
 import UIKit
 
 internal enum AppRadius: CGFloat {
-    case minimum
+    case minimum, normal, max
     func fgfg() -> CGFloat {
         switch self {
         case .minimum:
             return 6.0
+        case .normal:
+            return 6.0
+        case .max:
+            return 25.0
         }
     }
 }
--- Sources/DesignKit/Molecules/Onboarding.swift
+++ Sources/DesignKit/Molecules/Onboarding.swift
@@ -8,6 +8,6 @@ public struct OnboardingUI: OnboardingUIProtocol {
     public init(){}
     public func createNextScreenButton(text: String, action: @escaping () -> Void) -> UIButton {
         let font = FontFactory(font: .bold, size: .midium, color: .white100, alignment: .center)
-        return ActionButton.init(text: text, frontText: font, style: .positive, cornerRadius: .minimum, backgroundColor: .primary500, action: action).createButton()
+        return ActionButton.init(text: text, frontText: font, style: .positive, cornerRadius: .max, backgroundColor: .primary500, action: action).createButton()
     }
 }
