21ffa5404ff4df539e542f743380a4de3b8a3c0c
georgiiIaroshenko <georg2991@mail.ru>
2025-02-07 19:13:13 +0300

    no message

--- Sources/DesignKit/Atoms/Color.swift
+++ Sources/DesignKit/Atoms/Color.swift
@@ -56,52 +56,52 @@ internal enum DSColor {
     }
     private static let colorMap: [DSColor: UIColor] = [
         // Black
-        .black100:    loadColor("Colors/Black/black100"),
-        .black200:    loadColor("Colors/Black/black200"),
+        .black100:    loadColor("Colors/black100"),
+        .black200:    loadColor("Colors/black200"),
 
         // Green
-        .green100:    loadColor("Colors/Green/green100"),
-        .green200:    loadColor("Colors/Green/green200"),
-        .green300:    loadColor("Colors/Green/green300"),
+        .green100:    loadColor("Colors/green100"),
+        .green200:    loadColor("Colors/green200"),
+        .green300:    loadColor("Colors/green300"),
 
         // Neutral
-        .neutral100:  loadColor("Colors/Neutral/neutral100"),
-        .neutral200:  loadColor("Colors/Neutral/neutral200"),
-        .neutral300:  loadColor("Colors/Neutral/neutral300"),
-        .neutral400:  loadColor("Colors/Neutral/neutral400"),
-        .neutral500:  loadColor("Colors/Neutral/neutral500"),
-        .neutral600:  loadColor("Colors/Neutral/neutral600"),
-        .neutral700:  loadColor("Colors/Neutral/neutral700"),
-        .neutral800:  loadColor("Colors/Neutral/neutral800"),
-        .neutral900:  loadColor("Colors/Neutral/neutral900"),
+        .neutral100:  loadColor("Colors/neutral100"),
+        .neutral200:  loadColor("Colors/neutral200"),
+        .neutral300:  loadColor("Colors/neutral300"),
+        .neutral400:  loadColor("Colors/neutral400"),
+        .neutral500:  loadColor("Colors/neutral500"),
+        .neutral600:  loadColor("Colors/neutral600"),
+        .neutral700:  loadColor("Colors/neutral700"),
+        .neutral800:  loadColor("Colors/neutral800"),
+        .neutral900:  loadColor("Colors/neutral900"),
         
         // Primary
-        .primary100:  loadColor("Colors/Primary/primary100"),
-        .primary200:  loadColor("Colors/Primary/primary200"),
-        .primary300:  loadColor("Colors/Primary/primary300"),
-        .primary400:  loadColor("Colors/Primary/primary400"),
-        .primary500:  loadColor("Colors/Primary/primary500"),
-        .primary600:  loadColor("Colors/Primary/primary600"),
-        .primary700:  loadColor("Colors/Primary/primary700"),
-        .primary800:  loadColor("Colors/Primary/primary800"),
-        .primary900:  loadColor("Colors/Primary/primary900"),
+        .primary100:  loadColor("Colors/primary100"),
+        .primary200:  loadColor("Colors/primary200"),
+        .primary300:  loadColor("Colors/primary300"),
+        .primary400:  loadColor("Colors/primary400"),
+        .primary500:  loadColor("Colors/primary500"),
+        .primary600:  loadColor("Colors/primary600"),
+        .primary700:  loadColor("Colors/primary700"),
+        .primary800:  loadColor("Colors/primary800"),
+        .primary900:  loadColor("Colors/primary900"),
         
         // Red
         .red100:      loadColor("Colors/Red/red100"),
         
         // Secondary
-        .secondary100: loadColor("Colors/Secondary/secondary100"),
-        .secondary200: loadColor("Colors/Secondary/secondary200"),
-        .secondary300: loadColor("Colors/Secondary/secondary300"),
-        .secondary400: loadColor("Colors/Secondary/secondary400"),
-        .secondary500: loadColor("Colors/Secondary/secondary500"),
-        .secondary600: loadColor("Colors/Secondary/secondary600"),
-        .secondary700: loadColor("Colors/Secondary/secondary700"),
-        .secondary800: loadColor("Colors/Secondary/secondary800"),
-        .secondary900: loadColor("Colors/Secondary/secondary900"),
+        .secondary100: loadColor("Colors/secondary100"),
+        .secondary200: loadColor("Colors/secondary200"),
+        .secondary300: loadColor("Colors/secondary300"),
+        .secondary400: loadColor("Colors/secondary400"),
+        .secondary500: loadColor("Colors/secondary500"),
+        .secondary600: loadColor("Colors/secondary600"),
+        .secondary700: loadColor("Colors/secondary700"),
+        .secondary800: loadColor("Colors/secondary800"),
+        .secondary900: loadColor("Colors/secondary900"),
         
         // White
-        .white100:    loadColor("Colors/White/white100"),
+        .white100:    loadColor("Colors/white100"),
     ]
     
     private static func loadColor(_ assetName: String) -> UIColor {
