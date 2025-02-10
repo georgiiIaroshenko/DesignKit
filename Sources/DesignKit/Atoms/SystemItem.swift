cb88884429064f32e1eb0edd4c3e520064899662
georgiiIaroshenko <georg2991@mail.ru>
2025-02-10 11:44:16 +0300

    no message

--- Sources/DesignKit/Atoms/SystemItem.swift
+++ Sources/DesignKit/Atoms/SystemItem.swift
@@ -6,6 +6,7 @@ public enum SystemItem: String {
     case hands1 = "Hands1"
     case hands2 = "Hands2"
     case security = "Security"
+    case shieldNetwork = "ShieldNetwork"
     
     public var image: UIImage {
         return UIImage(named: self.rawValue, in: .module, compatibleWith: nil) ?? UIImage(systemName: "pencil")!
