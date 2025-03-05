4f8e856b0e4c73cf4371a0305f2db4fd046784ce
georgiiIaroshenko <georg2991@mail.ru>
2025-03-05 20:34:19 +0300

    no message

--- Sources/DesignKit/Atoms/DSSystemItem.swift
+++ Sources/DesignKit/Atoms/DSSystemItem.swift
@@ -1,12 +1,21 @@
 import UIKit
 
 
-public enum DSSystemItem: String {
+public enum DSSystemItemOnboarding: String {
     case  cupStar = "CupStar"
     case hands1 = "Hands1"
     case hands2 = "Hands2"
     case security = "Security"
     case shieldNetwork = "ShieldNetwork"
+    
+    public var image: UIImage {
+        return UIImage(named: self.rawValue, in: .module, compatibleWith: nil) ?? UIImage(systemName: "pencil")!
+    }
+}
+
+public enum DSSystemItemAuthReg: String {
+    case  eye = "Eye"
+    case eyeClosed = "EyeClosed"
     
     public var image: UIImage {
         return UIImage(named: self.rawValue, in: .module, compatibleWith: nil) ?? UIImage(systemName: "pencil")!
