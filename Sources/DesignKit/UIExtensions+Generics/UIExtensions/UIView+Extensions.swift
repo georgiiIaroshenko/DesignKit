69d1a3c64c3de4dbbca545c198ed89ffe468b20e
georgiiIaroshenko <georg2991@mail.ru>
2025-04-10 14:37:46 +0300

    no message

--- Sources/DesignKit/UIExtensions+Generics/UIExtensions/UIView+Extensions.swift
+++ Sources/DesignKit/UIExtensions+Generics/UIExtensions/UIView+Extensions.swift
@@ -1,6 +1,11 @@
 import UIKit
 
 extension UIView {
+    public func addSubviewsAndMaskFalse(_ subviews: [UIView]) {
+        self.addSubviews(subviews)
+        autoresizingMaskFalse(subviews)
+    }
+    
     public func addSubviews(_ subviews: [UIView]) {
             subviews.forEach { addSubview($0) }
         }
