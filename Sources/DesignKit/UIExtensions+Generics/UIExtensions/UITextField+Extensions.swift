fad87309edbd17535ec9de9574626413af17e19b
georgiiIaroshenko <georg2991@mail.ru>
2025-04-10 14:14:58 +0300

    no message

--- Sources/DesignKit/UIExtensions+Generics/UIExtensions/UITextField+Extensions.swift
+++ Sources/DesignKit/UIExtensions+Generics/UIExtensions/UITextField+Extensions.swift
@@ -4,5 +4,8 @@ extension UITextField: AttributedTextSettable {
     public func setAttributedContent(_ content: NSAttributedString) {
         self.attributedText = content
     }
-
+    
+    public func setAttributedContentPlaceholder(_ content: NSAttributedString) {
+        self.attributedPlaceholder = content
+    }
 }
