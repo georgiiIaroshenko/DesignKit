022cfab50aa9e2d735a9b3a2e92c16f0ae5d2968
georgiiIaroshenko <georg2991@mail.ru>
2025-04-10 13:58:39 +0300

    no message

--- Sources/DesignKit/UIExtensions+Generics/UIGenerics/UIGenerics.swift
+++ Sources/DesignKit/UIExtensions+Generics/UIGenerics/UIGenerics.swift
@@ -1,5 +1,5 @@
 import UIKit
 
-func autoresizingMaskFalse<T: UIView>(_ uiElement: [T]) {
+public func autoresizingMaskFalse<T: UIView>(_ uiElement: [T]) {
     uiElement.forEach { $0.translatesAutoresizingMaskIntoConstraints = false }
 }
