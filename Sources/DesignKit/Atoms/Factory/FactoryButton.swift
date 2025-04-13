6a4e93b646cfe66e435fbb01d1caf59fa3a043dd
georgiiIaroshenko <georg2991@mail.ru>
2025-04-13 13:08:16 +0300

    no message

--- Sources/DesignKit/Atoms/Factory/FactoryButton.swift
+++ Sources/DesignKit/Atoms/Factory/FactoryButton.swift
@@ -4,8 +4,20 @@ public struct ButtonConfiguration {
     let font: FontFactory
     let style: UIButton.ButtonType
     let cornerRadius: DSRadius
-    let backgroundColor: DSColor = .clear
+    let backgroundColor: DSColor
     let opacity: DSOpacity
+    
+    public init(font: FontFactory,
+                 style: UIButton.ButtonType,
+                 cornerRadius: DSRadius,
+                 backgroundColor: DSColor = .clear,
+                 opacity: DSOpacity){
+        self.font = font
+        self.style = style
+        self.cornerRadius = cornerRadius
+        self.backgroundColor = backgroundColor
+        self.opacity = opacity
+    }
 }
 
 public class FactoryButton {
