f0765fd2660e3ace06cee0bc3e143a59dae36b7d
georgiiIaroshenko <georg2991@mail.ru>
2025-04-17 17:41:01 +0300

    no message

--- Sources/DesignKit/Atoms/Factory/FactoryTextView.swift
+++ Sources/DesignKit/Atoms/Factory/FactoryTextView.swift
@@ -3,27 +3,27 @@ import UIKit
 public struct TextViewStyle {
     let font: FontFactory
     let backgroundColor: DSColor
-    let isEnabled: Bool
+    let isEditable: Bool
     let isScrollEnabled: Bool
     
-    public init(font: FontFactory, backgroundColor: DSColor, isEnabled: Bool, isScrollEnabled: Bool) {
+    public init(font: FontFactory, backgroundColor: DSColor, isEditable: Bool, isScrollEnabled: Bool) {
         self.font = font
         self.backgroundColor = backgroundColor
-        self.isEnabled = isEnabled
+        self.isEditable = isEditable
         self.isScrollEnabled = isScrollEnabled
     }
 }
 
-final public class FactoryTextView: UITextField {
-    public static func createTextField(_ style: TextViewStyle) -> UITextField {
-        let tf = UITextField()
-        tf.font = style.font.font.font(ofSize: style.font.size)
-        tf.textColor = style.font.color.uiColor
-        tf.textAlignment = style.font.alignment
-        tf.layer.opacity = style.font.opacity.rawValue
-        tf.backgroundColor = style.backgroundColor.uiColor
-        tf.isEnabled       = style.isEnabled
-//        tf.isScrollEnabled = style.isScrollEnabled
-        return tf
+final public class FactoryTextView: UITextView {
+    public static func createTextField(_ style: TextViewStyle) -> UITextView {
+        let sw = UITextView()
+        sw.font = style.font.font.font(ofSize: style.font.size)
+        sw.textColor = style.font.color.uiColor
+        sw.textAlignment = style.font.alignment
+        sw.layer.opacity = style.font.opacity.rawValue
+        sw.backgroundColor = style.backgroundColor.uiColor
+        sw.isEditable = style.isEditable
+        sw.isScrollEnabled = style.isScrollEnabled
+        return sw
     }
 }
