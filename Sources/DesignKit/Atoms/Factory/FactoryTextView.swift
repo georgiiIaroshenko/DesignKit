6a5b6436fbec5695e7e7f14418e2a0f090b59d6f
georgiiIaroshenko <georg2991@mail.ru>
2025-04-17 17:32:45 +0300

    no message

--- Sources/DesignKit/Atoms/Factory/FactoryTextView.swift
+++ Sources/DesignKit/Atoms/Factory/FactoryTextView.swift
@@ -3,21 +3,27 @@ import UIKit
 public struct TextViewStyle {
     let font: FontFactory
     let backgroundColor: DSColor
+    let isEditable: Bool
+    let isScrollEnabled: Bool
     
-    public init(font: FontFactory, backgroundColor: DSColor) {
+    public init(font: FontFactory, backgroundColor: DSColor, isEditable: Bool, isScrollEnabled: Bool) {
         self.font = font
         self.backgroundColor = backgroundColor
+        self.isEditable = isEditable
+        self.isScrollEnabled = isScrollEnabled
     }
 }
 
-final public class FactoryTextView: UITextField {
-    public static func createTextField(_ style: TextViewStyle) -> UITextField {
-        let sw = UITextField()
+final public class FactoryTextView: UITextView {
+    public static func createTextField(_ style: TextViewStyle) -> UITextView {
+        let sw = UITextView()
         sw.font = style.font.font.font(ofSize: style.font.size)
         sw.textColor = style.font.color.uiColor
         sw.textAlignment = style.font.alignment
         sw.layer.opacity = style.font.opacity.rawValue
         sw.backgroundColor = style.backgroundColor.uiColor
+        sw.isEditable = style.isEditable
+        sw.isScrollEnabled = style.isScrollEnabled
         return sw
     }
 }
